c0f33ae43306b2b30f66d12f1db172259c0c211e
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-05-09 23:46:59 +0200

    rename images

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,7 +1,7 @@
 import type { NextRequest } from "next/server";
 import { Ratelimit } from "@upstash/ratelimit";
 import { kv } from "@vercel/kv";
-import { getRandomImage } from "pages/images";
+import { getRandomImage } from "images";
 
 const ratelimit = new Ratelimit({
   redis: kv,
